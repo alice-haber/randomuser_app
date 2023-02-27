e28f43d2f4437ddc2ab01e645dc73cf52f69b757
alice-haber <aehaber33@gmail.com>
2023-02-27 09:37:59 -0500

    Added some comments to ensure decisions made thus far are documented

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,25 @@ import { getUser, relevantUserFields } from './getRandomUserAPI';
 import Grid from '@mui/material/Grid'
 import { Button, Divider, TextField } from '@mui/material';
 
+// Colors I liked from the OnSiteIQ homepage styles
 const onSiteIQWhite = '#fff'
 const onSiteIQPurple = '#680adc'
 
 function App() {
   const [user, setUser] = React.useState<relevantUserFields>(null)
+  // In a real project I would create a type to encapsulate the specific fields and restricted Action values
+  //  I expect. This is out of scope.
   const [approveHistory, setApproveHistory] = React.useState<Array<any>>()
 
   const resetUser = () => setUser(null)
 
+  // Pull a candidate on first render and pull a new one if user becomes null
   React.useEffect(() => {
     if (!user) {
       getUser()
         .then(setUser)}}, [user])
 
+  // Pull from localStorage and parse on first render
   React.useEffect(() => {
     const histString = localStorage.getItem('approveHistory')
     //Case where it's a string containing undefined which does not JSON.parse correctly
@@ -27,8 +32,8 @@ function App() {
     setApproveHistory(hist)
   }, [])
 
+  // Add a new entry to history, persist to storage, and reset user to force a new one to be pulled
   const handleCandidate = (action: 'Approve' | 'Reject') => () => {
-
     const newEntry = {message: '', user, action}
     const newApproveHistory = approveHistory
       ? approveHistory.concat(newEntry)
