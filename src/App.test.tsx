7265a0ba45906514a323a9bb6654d450df2c42cc
alice-haber <aehaber33@gmail.com>
2023-03-01 10:20:34 -0500

    Added reduced rejection test as well

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import { findAllByRole, fireEvent, getAllByRole, queryByAttribute, render, RenderResult, screen, waitFor } from '@testing-library/react'
 import App from './App'
 import fetchMock from "jest-fetch-mock"
+import { act } from 'react-dom/test-utils'
 
 import randomUserOne from './testData/randomUserOne.json'
-import { act } from 'react-dom/test-utils'
+import randomUserTwo from './testData/randomUserTwo.json'
 
 fetchMock.enableMocks();
 const getById = queryByAttribute.bind(null, 'id');
@@ -84,6 +85,41 @@ test('candidate info is displayed, user can leave comments and approve', async (
     expect(gridCells[1]).toHaveTextContent('Erstenyuk, RostislavaUkrainerostislava.erstenyuk@example.com')
     expect(gridCells[2]).toHaveTextContent('Approve')
     expect(gridCells[3]).toHaveTextContent('Rostislava is an excellent candidate.')
+  })
+
+})
+
+test('user can leave comments and reject', async () => {
+  fetchMock.mockResponse(JSON.stringify(randomUserTwo))
+  let dom: RenderResult
+
+  await act(() => {
+    dom = render(<App />)
+  })
+
+  await waitFor(() => {
+    const candidateFullNameElement = getById(dom.container, 'candidateFullName')
+    expect(candidateFullNameElement).toBeInTheDocument()
+  })
+
+  await act(() => {
+    typeMessage(dom, 'Jeannine was not a good fit.')
+    rejectClick(dom)
+  })
+
+  await waitFor(async () => {
+    //Input box should clear when "new" candidate is loaded, but message should appear in history view below
+    const candidateMessageInputElement = getById(dom.container, 'candidateMessageInput')!
+    expect (candidateMessageInputElement).toHaveTextContent('')
+
+    //Cells should be populated with the right info
+    const gridCells = await findAllByRole(dom.container, "cell");
+    const undoButton = gridCells[0].querySelector('button')
+
+    expect(undoButton).toBeInTheDocument()
+    expect(gridCells[1]).toHaveTextContent('Zacharias, JeannineGermanyjeannine.zacharias@example.com')
+    expect(gridCells[2]).toHaveTextContent('Reject')
+    expect(gridCells[3]).toHaveTextContent('Jeannine was not a good fit.')
   })
 
 })
